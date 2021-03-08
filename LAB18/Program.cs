c89d6148bc6cc0eaab6e8e7c7f5a3212cb87f97a
DustinOwens92 <DustinOwens92@gmail.com>
2021-03-08 18:23:02 -0500

    Initial check-in of module LAB18

---
+++ LAB18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var list = new List(10);
+
+            list.InsertAt(0, "Hello");
+            list.PrintReverse();
+
+
+            Console.WriteLine(list.InsertAt(5, " there!"));
+            list.PrintReverse();
+
+            Console.WriteLine(list.RemoveAt(0));
+
+            list.PrintReverse();
+
+        }
+    }
+}
+
+
